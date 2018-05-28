397dcd1e190cc95584595134b948b7f2da049bad
vinogradovnick32 <vinogradovnick32@gmail.com>
2018-05-28 15:54:54 +0500

    add extension

--- EducationalPratice_Task_11/Encrypt.cs
+++ EducationalPratice_Task_11/Encrypt.cs
@@ -12,18 +12,10 @@ namespace EducationPratice_Task_11
         /// Входная строка
         /// </summary>
         private string InputString { get; set; }
-        private static int InputNumerator=0;
-        private const int MatrixLength = 5;//Размерность кодируемой матрицы
         private List<string[,]> EncryptionTable { get; set; }
-        private List<int> keysList= new List<int>();
-        private static Random rd= new Random();
-        private static int keycount = 0;
-        /// <summary>
-        /// Перечислитель списка ключей
-        /// </summary>
-        private static int KeyNumerator = 0;
-
-        private static int n;
+        private int MatrixLength;
+        private List<int> key;
+        private Random rd = new Random();
         /// <summary>
         /// Конструктор класса
         /// </summary>
@@ -31,184 +23,37 @@ namespace EducationPratice_Task_11
         public Encrypt(string text)
         {
             InputString = text;//Входная строка
+            key= new List<int>(InputString.Length/4);
             EncryptionTable=new List<string[,]>();
+            MatrixLength = 10;
         }
-        /// <summary>
-        /// Генерация массива ключей для поиска
-        /// </summary>
-        /// <returns></returns>
-        public List<int> keyGenerate()
+
+        private void KeyGen()
         {
-            n = keycount == 3 ? 7 : 6;
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < 25; i++)
             {
-                int number = rd.Next(1, 26);
-
-                while (keysList.Contains(number))
-                    number = rd.Next(1, 26);
-                keysList.Add(number);
+               int number = rd.Next(1, 100);
+                while (key.Contains(number))
+                    number = rd.Next(1,100);
+                key.Add(number);
             }
-
-            keycount++;
-            return keysList;
-        }
-        /// <summary>
-        /// Заполнение матрицы числами
-        /// </summary>
-        /// <param name="matrix"></param>
-        /// <returns></returns>
-        public int[,] FillMatrixNumbers(int[,] matrix)
-        {
-            int number = 1;
-            for (int i = 0; i < matrix.GetLength(1); i++)
-            {
-                for (int j = 0; j < matrix.GetLength(0); j++)
-                {
-                    matrix[i, j] = number++;
-                }
-            }
-            return matrix;
-        }
-
-        public void GeneratePrintKeys()
-        {
-            foreach (int item in this.keyGenerate())
-                Console.WriteLine(item);
-        }
-
-        public int[,] PartMatrix()
-        {
-            GeneratePrintKeys();
-            int[,] part = new int[MatrixLength, MatrixLength];
-            int[] key = new int[n];
-
-            Array.Copy(keysList.ToArray(),KeyNumerator,key,0,n);
-            Console.WriteLine("Ключи");
-        
-            foreach (int item in key)
-            {
-                Console.Write($"{item} ");
-            }
-            Console.WriteLine();
-
-            KeyNumerator += n;
-            
-            KeyFill(FillMatrixNumbers(part),key.ToList());
-            PrintMatrix(part);
-            return part;
-        }
-
-        string[,] MatrixFillChars(int[,] matrix)
-        {
-            string[,] temp = new string[matrix.GetLength(1),matrix.GetLength(0)];
-            
-
-            for (int i = temp.GetLength(1) - 1; i >= 0; i--)
-            {
-                for (int j = 0; j < temp.GetLength(0); j++)
-                {
-                    if (matrix[i, j] == 0)
-                    {
-                        temp[i, j] = InputString[InputNumerator].ToString();
-                        InputNumerator++;
-                        Console.WriteLine(temp[i,j] +" "+InputNumerator);
-                    }
-                    else
-                        temp[i, j] = matrix[i, j].ToString();
-                }
-            }
-            return temp;
-        }
-
-        string[,] MatrixFillChars(string[,] matrix)
-        {
-            string[,] temp = new string[matrix.GetLength(1), matrix.GetLength(0)];
-                for (int i = temp.GetLength(1) - 1; i >= 0; i--)
-                {
-                    for (int j = 0; j < temp.GetLength(0); j++)
-                    {
-                        if (Intparse(matrix[i, j]) == 0)
-                        {
-                            temp[i, j] = InputString[InputNumerator].ToString();
-                            Console.WriteLine(temp[i, j]+" "+InputNumerator);
-                            InputNumerator++;
-                        }
-                        else
-                            temp[i, j] = matrix[i, j];
-                    }
-                }
-            return temp;
-        }
-
-        string[,] ConcatMatrix(string[,] first, int[,] second)
-        {
-            for (int i = 0; i < first.GetLength(1); i++)
-            {
-                for (int j = 0; j < first.GetLength(0); j++)
-                {
-                    int res = 0;
-                    if (second[i, j] == 0)
-                        first[i, j] = "0";
-                }
-            }
-
-            return first;
-        }
-
-        bool parse(string obj)
-        {
-            if (obj is null) return false;
-            Console.WriteLine(obj);
-            int res = 0;
-            return int.TryParse(obj.ToString(),out res);
-        }
-        int Intparse(string obj)
-        {
-            
-            Console.WriteLine(obj);
-            int res = 0;
-            int.TryParse(obj.ToString(), out res);
-            return res;
         }
         public void Encryption()
         {
-            /*
-             * Матрица обычная, заполняем нулями по ключам
-             *
-             */
-           
+            int[,]table = new int[MatrixLength,MatrixLength];
+            table.Fill();
+            PrintMatrix(table);
+            KeyGen();
+            table.KeyTableFill(key);
+            PrintMatrix(table);
 
-            int[,] basic = BuildEncryptMatrix(PartMatrix(), PartMatrix(), PartMatrix(), PartMatrix());
-            Console.WriteLine("Шараш-матрица-монтаж");
-            int[,] matrix90 = RotateMatrix(basic);
-            int[,] matrix180 = RotateMatrix(matrix90);
-            int[,] matrix270 = RotateMatrix(matrix180);
-            Console.WriteLine("basic");
-            PrintMatrix(basic);
-            Console.WriteLine("matrix 90");
-            PrintMatrix(matrix90);
-            Console.WriteLine("matrix 180");
-            PrintMatrix(matrix180);
-            Console.WriteLine("matrix 270");
-            PrintMatrix(matrix270);
-          
 
         }
-        
-        /// <summary>
-        /// Заполнение матрицы ключами
-        /// </summary>
-        /// <param name="array">матрица для шифрования</param>
-        /// <param name="key">массив ключей для шифрования данного массива</param>
-        /// <returns></returns>
-        static int[,] KeyFill(int[,] array,List<int>key)
-        {
-            for (int i = 0; i < array.GetLength(1); i++)
-            for (int j = 0; j < array.GetLength(0); j++)
-                if (key.Contains(array[i, j]))
-                    array[i, j] = 0;
-            return array;
-        }
+       
+     
+   
+
+
         /// <summary>
         /// Поворот матрицы на 90 градусов
         /// </summary>
@@ -224,95 +69,8 @@ namespace EducationPratice_Task_11
             return rotated_matrix;
         }
 
-        string[,] WordFill(int[,] matrix)
-        {
-            string[,] array=  new string[matrix.GetLength(1),matrix.GetLength(0)];
-            int number = 0;
-            for (int i = 0; i < matrix.GetLength(1); i++)
-            {
-                for (int j = 0; j < matrix.GetLength(0); j++)
-                {
-                    if (matrix[i, j] == 0)
-                        array[i, j] = InputString[number++].ToString();
-                    else
-                        array[i, j] = matrix[i, j].ToString();
-                }
-            }
-            Console.WriteLine();
-            for (int i = 0; i < array.GetLength(1); i++)
-            {
-                for (int j = 0; j < array.GetLength(0); j++)
-                {
-                    Console.Write($" {array[i,j],3} ");
-                }
-                Console.WriteLine();
-            }
-
-            return array;
-        }
-        /// <summary>
-        /// Создание шифрованой таблицы 10х10
-        /// </summary>
-        /// <param name="basic">начальное положение</param>
-        /// <param name="right">матрица справа</param>
-        /// <param name="rightdown">матрица справа снизу</param>
-        /// <param name="down"> матрица снизу</param>
-        /// <returns></returns>
-        static int[,] BuildEncryptMatrix(int[,] basic, int[,] right, int[,] rightdown, int[,] down)
-        {
-            int[,] temp = new int[basic.GetLength(1)*2,basic.GetLength(0)*2];
-            try
-            {
-                Console.WriteLine("basic");
-                for (int i = 0; i < basic.GetLength(1); i++)
-                    for (int j = 0; j < basic.GetLength(0); j++)
-                        temp[i, j] = basic[i, j];
-                PrintMatrix(temp);
-                Console.WriteLine("Заполнение 2 матрицы справа");
-                //Заполнение 2 матрицы справа
-
-                for (int i = 0; i < basic.GetLength(1); i++)
-                for (int j = basic.GetLength(0); j < basic.GetLength(0) + right.GetLength(0); j++)
-                    temp[i, j] = right[i, j - basic.GetLength(0)];
-                PrintMatrix(temp);
-
-               
-                Console.WriteLine("Заполнение 3 матрицы снизу");
-                //Заполнение 3 матрицы снизу
-
-                for (int i = basic.GetLength(1); i < basic.GetLength(1) * 2; i++)
-                {
-                    for (int j = 0; j < down.GetLength(0); j++)
-                        temp[i, j] = down[i - basic.GetLength(1), j];
-                }
-                PrintMatrix(temp);
-                Console.WriteLine("Заполнение 4 матрицы справа снизу");
-                //Заполнение 4 матрицы справа снизу
-                for (int i = basic.GetLength(1); i < basic.GetLength(1) + rightdown.GetLength(1); i++)
-                    for (int j = basic.GetLength(0); j < basic.GetLength(0) + rightdown.GetLength(0); j++)
-                        temp[i, j] = rightdown[i - basic.GetLength(1), j - basic.GetLength(0)];
-                PrintMatrix(temp);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-
-            }
-
-            return temp;
-        }
-        /// <summary>
-        /// Заполнение матрицы
-        /// </summary>
-        /// <param name="a">входной массив</param>
-        /// <param name="size">Размер матрицы</param>
-            static void FillMatrix(int[,] matrix,string word)
-        {
-            int counter = 0;
-            for (int i = 0; i < matrix.GetLength(0); i++)
-            for (int j = 0; j < matrix.GetLength(1); j++)
-                matrix[i, j] = word[counter++];
-        }
+     
+    
         /// <summary>
         /// Печать матрицы
         /// </summary>
@@ -341,6 +99,53 @@ namespace EducationPratice_Task_11
             }
         }
 
+    }
+
+    public static class EncryptTableExtension
+    {
+        public static int[,] Fill(this int[,] array)
+        {
+
+
+            int number = 1;
+            for (int i = 0; i <array.GetLength(1); i++)
+            {
+                for (int j = 0; j <array.GetLength(0) ; j++)
+                {
+                    array[i, j] = number++;
+                }   
+            }
+
+            return array;
+        }
+
+        public static int[,] KeyTableFill(this int[,]array, List<int> key)
+        {
+            for (int i = 0; i < array.GetLength(1); i++)
+            for (int j = 0; j < array.GetLength(0); j++)
+                if (key.Contains(array[i, j]))
+                    array[i, j] = 0;
+
+            return array;
+        }
+
+        public static string[,] ConcatTables(this string[,] first, int[,] second)
+        {
+            for (int i = 0; i < first.GetLength(1); i++)
+            {
+                for (int j = 0; j < first.GetLength(0); j++)
+                {
+                    int number = -1;
+
+                    bool status = int.TryParse(first[i,j], out number);
+                    if (status && number == second[i, j])
+                        first[i, j] = "0";
+                }
+            }
+
+            return first;
+        }
+       
     }
 
 }
